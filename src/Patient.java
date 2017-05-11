8092550ec082181c50ee11777dc5c5db5983a14a
amenvafan <elisabet_lilja@yahoo.se>
2017-05-11 15:18:24 +0200

    liten fix

--- src/Patient.java
+++ src/Patient.java
@@ -1,54 +1,59 @@
 import java.awt.Color;
-import java.io.File;
-import java.util.Scanner;
+
 
 public class Patient {
 	public int eKey;
 	public String name;
-<<<<<<< HEAD
+
 	public String diagnos;
 	public Color emergencyCol;
-	
-	
-	public Patient(int eKey, String diagnos, String name){
-=======
-	
-	public Patient(int eKey, String name){
->>>>>>> origin/master
+
+	public Patient(int eKey, String diagnos, String name) {
+
 		this.name = name;
 		this.eKey = eKey;
 		this.diagnos = diagnos;
-		
+
 	}
-	
-		public String toString(){
-		return name+ " " + diagnos + " " + eKey;
-	}	
-		public int geteKey(){
-			return eKey;
+	public Patient(int eKey,  String name) {
+
+		this.name = name;
+		this.eKey = eKey;
+
+	}
+
+	public String toString() {
+		return name + " " + diagnos + " " + eKey;
+	}
+
+	public int geteKey() {
+		return eKey;
+	}
+
+	public String getDiagnos() {
+		return diagnos;
+	}
+
+	public Color emergency(int prio) {
+		if (prio == 0) {
+			emergencyCol = new Color(0, 255, 0);
+			return emergencyCol;
 		}
-		
-		public Color emergency(int prio){
-			if(prio == 0){
-				emergencyCol = new Color (0, 255, 0);
+
+		else if (prio == 1) {
+			emergencyCol = new Color(64, 224, 208);
 			return emergencyCol;
-			}
-			
-			else if(prio == 1){
-				emergencyCol = new Color (64, 224, 208);
-			return emergencyCol;
-			}
-			
-			else if(prio == 2){
-				emergencyCol = new Color (0, 0, 255);
-			return emergencyCol;
-			}
-			
-			else
-				emergencyCol = new Color (255, 0, 0);
-			return emergencyCol;
-			
-			
 		}
-		
+
+		else if (prio == 2) {
+			emergencyCol = new Color(0, 0, 255);
+			return emergencyCol;
+		}
+
+		else
+			emergencyCol = new Color(255, 0, 0);
+		return emergencyCol;
+
+	}
+
 }
