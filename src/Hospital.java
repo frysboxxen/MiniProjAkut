7f7ab72e50448060f7afd32482da3f5fc3555690
flixon <flixon@me.com>
2017-05-11 11:17:44 +0200

    que kö

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,50 +1,54 @@
 
 public class Hospital {
 	public static void main(String[] args) {
+		StdDraw.enableDoubleBuffering();
+		int xLed = 1600;
+		int yLed = 800;
+		StdDraw.setCanvasSize(xLed, yLed);
 		Patient p1 = new Patient(0, "kalle");
 		Patient p2 = new Patient(3, "karl");
 
 		
-		PatientQueue kö = new PatientQueue(11);
-		kö.addPatient(p1);
-		kö.addPatient(p2);
-		kö.addPatient(new Patient(2,"felix"));
-		kö.addPatient(new Patient(2,"marcus"));
-		kö.addPatient(new Patient(3,"oscar"));
-		kö.addPatient(new Patient(2,"elias"));
-		kö.addPatient(new Patient(2,"inez"));
-		kö.addPatient(new Patient(1,"magdalena"));
-		kö.addPatient(new Patient(1,"daniel"));
-		kö.addPatient(new Patient(0,"simon"));
+		PatientQueue que = new PatientQueue(11);
+		que.addPatient(p1);
+		que.addPatient(p2);
+		que.addPatient(new Patient(2,"felix"));
+		que.addPatient(new Patient(2,"marcus"));
+		que.addPatient(new Patient(3,"oscar"));
+		que.addPatient(new Patient(2,"elias"));
+		que.addPatient(new Patient(2,"inez"));
+		que.addPatient(new Patient(1,"magdalena"));
+		que.addPatient(new Patient(1,"daniel"));
+		que.addPatient(new Patient(0,"simon"));
 
-		kö.print();
-		System.out.println();
-		System.out.println("Sort:");
-		System.out.println();
-		kö.sort();
-		kö.print();
-		System.out.println();
-		System.out.println("Remove");
-		System.out.println();
-		System.out.println(kö.pop().toString());
-		System.out.println();
-		kö.print();
-		//PatientIterator u = new PatientIterator[4];
-		PatientIterator b = kö.iterator();
-		System.out.println("iterator print");
-		while(b.hasNext()){
-			System.out.println(b.next().toString());
-		}
+//		que.print();
+//		System.out.println();
+//		System.out.println("Sort:");
+//		System.out.println();
+//		que.sort();
+//		que.print();
+//		System.out.println();
+//		System.out.println("Remove");
+//		System.out.println();
+//		System.out.println(que.pop().toString());
+//		System.out.println();
+//		que.print();
+		PatientIterator[] u = new PatientIterator[4];
 		
-		
-		/*for(int i = 0; i < 4; i++){
-			u[i] = kö.iterator();
-		}
-		
+		StdDraw.show();
 		for(int i = 0; i < 4; i++){
+			u[i] = que.iterator();
+		}
+		
+		for(int i = 3; i >= 0; i--){
 			while(u[i].hasNext()){
-				if(u[i].)
+				Patient tmp = u[i].next();
+				if(tmp.eKey == i){
+					System.out.println(tmp.toString());
+				}
 			}
-		}*/
+		}
+		StdDraw.clear();
+		
 	}
 }
