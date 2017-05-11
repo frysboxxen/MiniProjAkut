83ee0d666e6915d4c97572680b30e310a02dc34c
flixon <flixon@me.com>
2017-05-11 12:55:40 +0200

    Felix

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,15 +1,18 @@
+import java.awt.Color;
 
 public class Hospital {
 	public static void main(String[] args) {
-		StdDraw.enableDoubleBuffering();
+//		StdDraw.enableDoubleBuffering();
 		int xLed = 1600;
 		int yLed = 800;
 		StdDraw.setCanvasSize(xLed, yLed);
+		StdDraw.setXscale(0,xLed);
+		StdDraw.setYscale(0,yLed);
 		Patient p1 = new Patient(0, "kalle");
 		Patient p2 = new Patient(3, "karl");
 
 		
-		PatientQueue que = new PatientQueue(11);
+		PatientQueue que = new PatientQueue(12);
 		que.addPatient(p1);
 		que.addPatient(p2);
 		que.addPatient(new Patient(2,"felix"));
@@ -20,6 +23,7 @@ public class Hospital {
 		que.addPatient(new Patient(1,"magdalena"));
 		que.addPatient(new Patient(1,"daniel"));
 		que.addPatient(new Patient(0,"simon"));
+		que.addPatient(new Patient(3,"elisabeth"));
 
 //		que.print();
 //		System.out.println();
@@ -40,15 +44,21 @@ public class Hospital {
 			u[i] = que.iterator();
 		}
 		
+		int j = 1;
 		for(int i = 3; i >= 0; i--){
 			while(u[i].hasNext()){
 				Patient tmp = u[i].next();
 				if(tmp.eKey == i){
 					System.out.println(tmp.toString());
+					StdDraw.setPenColor(new Color (i*50,i*50,i*50));
+					StdDraw.setPenRadius(0.01);
+					StdDraw.ellipse((j*xLed)/(que.size()*2), 100, 60, 40);
+					j = j +2;
 				}
 			}
 		}
-		StdDraw.clear();
+		StdDraw.show();
+//		StdDraw.clear();
 		
 	}
 }
